91d26f79e600b7ce0e104788451e12b7b0e6af52
WioDari <118029870+WioDari@users.noreply.github.com>
2025-10-09 16:28:28 +0300

    Add files via upload

---
+++ DemoExam/Models/Tovar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoExam.Models
+{
+    public class Tovar
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string art { get; set; }
+        public string name { get; set; }
+        public string dim { get; set; }
+        public int price { get; set; }
+        public string supplier { get; set; }
+        public string creator { get; set; }
+        public string category { get; set; }
+        public int discount { get; set; }
+        public int quantity { get; set; }
+        public string description { get; set; }
+        public string photo { get; set; }
+    }
+}
